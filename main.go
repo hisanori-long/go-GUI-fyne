335b158c86e1ff48403d57caed9e7385fce48e31
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-05-26 03:13:04 +0900

    first-commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"strconv"
+
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/theme"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+	l := widget.NewLabel("Hello Fyne!")
+	e := widget.NewEntry()
+	w.SetContent(
+		widget.NewVBox(
+			l, e,
+			widget.NewButton("Click me!", func() {
+				n, _ := strconv.Atoi(e.Text)
+				l.SetText("Total: " + strconv.Itoa(total(n)))
+			}),
+		),
+	)
+	a.Settings().SetTheme(theme.DarkTheme())
+	w.ShowAndRun()
+}
+
+func total(n int) int {
+	t := 0
+	for i := 1; i <= n; i++ {
+		t += i
+	}
+	return t
+}
