0bbfddcf6e63c6db646a67285d43e5b77ce1a0d4
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-05-28 23:41:30 +0900

    scroll

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fyne.io/fyne"
 	"fyne.io/fyne/app"
-	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 )
 
@@ -11,21 +9,19 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 	w.SetContent(
-		fyne.NewContainerWithLayout(
-			layout.NewGridLayout(3),
-			widget.NewButton("One", nil),
-			widget.NewButton("Two", nil),
-			widget.NewButton("Three", nil),
-			widget.NewButton("Four", nil),
-			layout.NewSpacer(),
-			widget.NewButton("Five", nil),
-			widget.NewButton("Six", nil),
-			widget.NewButton("Seven", nil),
-			widget.NewButton("Eight", nil),
-			layout.NewSpacer(),
-			widget.NewButton("Nine", nil),
-			widget.NewButton("Ten", nil),
-			widget.NewButton("Eleven", nil),
+		widget.NewScrollContainer(
+			widget.NewVBox(
+				widget.NewButton("One", nil),
+				widget.NewButton("Two", nil),
+				widget.NewButton("Three", nil),
+				widget.NewButton("Four", nil),
+				widget.NewButton("Five", nil),
+				widget.NewButton("Six", nil),
+				widget.NewButton("Seven", nil),
+				widget.NewButton("Eight", nil),
+				widget.NewButton("Nine", nil),
+				widget.NewButton("Ten", nil),
+			),
 		),
 	)
 	w.ShowAndRun()
