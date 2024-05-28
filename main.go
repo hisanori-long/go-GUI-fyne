aafd3c919a2b2b277e9b84be079acc700abb22ab
Hisanori Shimakawa <shimakawa.hisanori@gmail.com>
2024-05-28 23:58:11 +0900

    main menu

--- main.go
+++ main.go
@@ -3,30 +3,29 @@ package main
 import (
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
-	"fyne.io/fyne/layout"
-	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
-	l := widget.NewLabel("This is Sample widget.")
-	tb := widget.NewToolbar(
-		widget.NewToolbarAction(theme.HomeIcon(), func() {
-			l.SetText("Select Home Icon!")
-		}),
-		widget.NewToolbarAction(theme.InfoIcon(), func() {
-			l.SetText("Select Info Icon!")
-		}),
+	l := widget.NewLabel("Hello Fyne!")
+
+	mm := fyne.NewMainMenu(
+		fyne.NewMenu("File",
+			fyne.NewMenuItem("New", func() {
+				l.SetText("select 'New' menu item.")
+			}),
+			fyne.NewMenuItem(("Quit"), func() {
+				a.Quit()
+			}),
+		),
 	)
+	w.SetMainMenu(mm)
 	w.SetContent(
-		fyne.NewContainerWithLayout(
-			layout.NewBorderLayout(
-				nil, tb, nil, nil,
-			),
-			tb,
+		widget.NewVBox(
 			l,
+			widget.NewButton("ok", nil),
 		),
 	)
 	w.Resize(fyne.NewSize(300, 200))
